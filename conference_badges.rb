e1cbc2d459e094c201d3416b171bfb26fba28207
john beltrez <beltrezjohn@gmail.com>
2020-08-03 22:19:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,23 +10,23 @@ def batch_badge_creator(attendees)
   badges
 end
 
-def assign_rooms(attendees)
+def assign_rooms(speakers)
   room_assignments = []
-  attendees.each_with_index do |name, index|
-  room_assignments << ("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+  speakers.each_with_index do |name, index|
+    room_assignments << ("Hello, #{name}! You'll be assigned to room #{index + 1}!")
   end
-room_assignments
-end
+  room_assignments
+end 
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
-  end
+  end 
   
   assign_rooms(attendees).each do |assignment|
     puts assignment
   end
 end
-
-
+  
+  
 
